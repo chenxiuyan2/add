d01ad7320088cb8efc4aaeb87be4b99e5059f164
chenxiuyan2 <91605550+chenxiuyan2@users.noreply.github.com>
2021-10-06 14:49:19 +0900

    Add files via upload

---
+++ src/test/java/exe/Test2.java
@@ -0,0 +1,11 @@
+package exe;
+
+public class Test2 {
+
+  public static void main(String[] args) {
+    String message = "my uncle's dog eats nothing \\. ";
+    System.out.println(message);
+
+  }
+
+}
